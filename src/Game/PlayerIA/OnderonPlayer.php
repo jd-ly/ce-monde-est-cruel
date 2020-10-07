ab7eb349c984d149a93a43c76d9931e1b2537f53
Jean Damien Ly <jean.damien.ly@hotmail.fr>
2020-10-07 20:04:22 +0200

    Initial commit

--- src/Game/PlayerIA/OnderonPlayer.php
+++ src/Game/PlayerIA/OnderonPlayer.php
@@ -14,35 +14,49 @@ class OnderonPlayer extends Player
     protected $mySide;
     protected $opponentSide;
     protected $result;
+    protected $beat = ['parent::rockChoice()', 'parent::paperChoice()', 'parent::scissorsChoice()'];
 
     public function getChoice()
     {
-        // -------------------------------------    -----------------------------------------------------
-        // How to get my Last Choice           ?    $this->result->getLastChoiceFor($this->mySide) -- if 0 (first round)
-        // How to get the opponent Last Choice ?    $this->result->getLastChoiceFor($this->opponentSide) -- if 0 (first round)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get my Last Score            ?    $this->result->getLastScoreFor($this->mySide) -- if 0 (first round)
-        // How to get the opponent Last Score  ?    $this->result->getLastScoreFor($this->opponentSide) -- if 0 (first round)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get all the Choices          ?    $this->result->getChoicesFor($this->mySide)
-        // How to get the opponent Last Choice ?    $this->result->getChoicesFor($this->opponentSide)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get my Last Score            ?    $this->result->getLastScoreFor($this->mySide)
-        // How to get the opponent Last Score  ?    $this->result->getLastScoreFor($this->opponentSide)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get the stats                ?    $this->result->getStats()
-        // How to get the stats for me         ?    $this->result->getStatsFor($this->mySide)
-        //          array('name' => value, 'score' => value, 'friend' => value, 'foe' => value
-        // How to get the stats for the oppo   ?    $this->result->getStatsFor($this->opponentSide)
-        //          array('name' => value, 'score' => value, 'friend' => value, 'foe' => value
-        // -------------------------------------    -----------------------------------------------------
-        // How to get the number of round      ?    $this->result->getNbRound()
-        // -------------------------------------    -----------------------------------------------------
-        // How can i display the result of each round ? $this->prettyDisplay()
-        // -------------------------------------    -----------------------------------------------------
-
-
-        return parent::rockChoice();
-
+        $scores = $this->result->getScoresFor($this->mySide);
+        $length = count($scores);
+        if ($length == 0) {
+            return parent::rockChoice();
+        }
+        $myLastChoice = $this->result->getLastChoiceFor($this->mySide);
+        $opponentLastChoice = $this->result->getLastChoiceFor($this->opponentSide);
+        switch ($scores[$length - 1]) {
+            case 0: //lost
+                switch ($opponentLastChoice) {
+                    case 'paper':
+                        return parent::scissorsChoice();
+                    case 'rock':
+                        return parent::paperChoice();
+                    case 'scissor':
+                        return parent::rockChoice();
+                }
+                break;
+            case 5: // won
+                switch ($myLastChoice) {
+                    case 'paper':
+                        return parent::scissorsChoice();
+                    case 'rock':
+                        return parent::paperChoice();
+                    case 'scissor':
+                        return parent::rockChoice();
+                }
+                break;
+            case 1: //tie
+                switch ($myLastChoice) {
+                    case 'paper':
+                        return parent::scissorsChoice();
+                    case 'rock':
+                        return parent::paperChoice();
+                    case 'scissor':
+                        return parent::rockChoice();
+                }
+                break;
+        }
+        return parent::paperChoice();
     }
 };
